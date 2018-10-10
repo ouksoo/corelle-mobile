fac7b380069217a1173158d1e9c6bfa6fc907cb4
DESKTOP-U1PUFMN\hp <ouksoo@nate.com>
2018-10-10 16:37:45 +0900

    - 거의 완료 + 1

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,11 +1,169 @@
+let appendHeader = '';
+appendHeader += '<h1>Corelle Brands</h1>';
+appendHeader += '<a href="http://shop.worldkitchen.co.kr/shop/main/index.php" class="zoom">쇼핑몰 바로가기</a>';
+appendHeader += '<a href="#" class="menu">';
+appendHeader += '   <span class="f"></span>';
+appendHeader += '   <span class="s"></span>';
+appendHeader += '</a>';
+
+let appendContent = '';
+appendContent += '<div class="film-dark" style="display: none"></div>';
+appendContent += '<a href="#" class="go-top">위로가기</a>';
+
+appendContent += '<div class="submenu" style="display: none">';
+appendContent += '    <span class="logo"></span>';
+appendContent += '    <a href="#" class="close-modal"></a>';
+appendContent += '    <div class="inner">';
+appendContent += '        <ul class="clearfix navi">';
+appendContent += '            <li><a href="#" data-gnb-menu="about">About Us</a></li>';
+appendContent += '            <li><a href="#" data-gnb-menu="brand">Brands</a></li>';
+appendContent += '            <li><a href="#" data-gnb-menu="product">Product</a></li>';
+appendContent += '            <li><a href="#" data-gnb-menu="support">Support</a></li>';
+appendContent += '            <li><a href="#" data-gnb-menu="news">News & Event</a></li>';
+appendContent += '            <li><a href="#" data-gnb-menu="webzine">Webzine</a></li>';
+appendContent += '        </ul>';
+appendContent += '        <a href="http://shop.worldkitchen.co.kr/shop/main/index.php" target="_blank" class="shop">Online-shop 바로가기</a>';
+appendContent += '        <ul class="clearfix sns">';
+appendContent += '            <li><a href="https://www.facebook.com/Corellebrandskorea" target="_blank" class="facebook">페이스북</a></li>';
+appendContent += '            <li><a href="https://www.instagram.com/Corellebrands.kr" target="_blank" class="insta">인스타그램</a></li>';
+appendContent += '            <li><a href="https://www.youtube.com/channel/UCpsehKNlLWnBRTqOUAKikkg" target="_blank" class="youtube">유튜브</a></li>';
+appendContent += '        </ul>';
+appendContent += '    </div>';
+appendContent += '</div>';
+
+appendContent += '<div class="depthmenu" style="display: none">';
+appendContent += '    <a href="#" class="home">Home</a>';
+appendContent += '    <a href="#" class="close-modal"></a>';
+appendContent += '    <span class="depth-1">About Us</span>';
+appendContent += '    <div class="inner">';
+appendContent += '        <ul id="menu-about" class="clearfix navi" style="display:none">';
+appendContent += '            <li><a href="about_history.html">코렐 브랜드 소개/역사</a></li>';
+appendContent += '            <li><a href="about_contact.html">회사위치 및 정보</a></li>';
+appendContent += '            <li><a href="about_video.html">홍보영상 및 보도자료</a></li>';
+appendContent += '        </ul>';
+appendContent += '        <ul id="menu-brand" class="clearfix navi" style="display:none">';
+appendContent += '            <li><a href="brand_corelle.html">Corelle</a></li>';
+appendContent += '            <li><a href="brand_pyrex.html">Pyrex</a></li>';
+appendContent += '            <li><a href="brand_corning.html">Corningware</a></li>';
+appendContent += '            <li><a href="brand_snapware.html">Snapware</a></li>';
+appendContent += '            <li><a href="brand_chicago.html">Chicago Cutlery</a></li>';
+appendContent += '            <li><a href="brand_vision.html">Visions</a></li>';
+appendContent += '            <li><a href="brand_cook.html">Corelle cook</a></li>';
+appendContent += '            <li><a href="brand_oxo.html">OXO</a></li>';
+appendContent += '        </ul>';
+appendContent += '        <ul id="menu-product" class="clearfix navi" style="display:none">';
+appendContent += '            <li><a href="product_corelle.html">제품소개</a></li>';
+appendContent += '        </ul>';
+appendContent += '        <ul id="menu-support" class="clearfix navi" style="display:none">';
+appendContent += '            <li><a href="support_faq.html">FAQ</a></li>';
+appendContent += '            <li><a href="support_asking.html">1:1문의</a></li>';
+appendContent += '            <li><a href="support_caution.html">사용법 및 주의사항</a></li>';
+appendContent += '            <li><a href="support_store.html">매장안내</a></li>';
+appendContent += '        </ul>';
+appendContent += '        <ul id="menu-news" class="clearfix navi" style="display:none">';
+appendContent += '            <li><a href="news_list.html">새로운 소식</a></li>';
+appendContent += '            <li><a href="event_list.html">이벤트</a></li>';
+appendContent += '        </ul>';
+appendContent += '        <ul id="menu-webzine" class="clearfix navi" style="display:none">';
+appendContent += '            <li><a href="http://www.worldkitchen.co.kr/tabletelling/pc/index.php?channel=4" target="_blank">테이블 텔링</a></li>';
+appendContent += '            <li><a href="https://www.corellebrands.co.kr/MSNY/pc/main.html?channel=2" target="_blank">마케스트리트 뉴욕</a></li>';
+appendContent += '            <li><a href="https://www.corellebrands.co.kr/pyrex/pc/main.html?channel=2" target="_blank">파이렉스의 키친놀로지</a></li>';
+appendContent += '        </ul>';
+appendContent += '    </div>';
+appendContent += '</div>';
+appendContent += '<div id="use-term" class="popup-term" style="display: none"> <a href="#" class="close-modal"></a> <span class="s-tit">이용약관</span> <div class="content"> 제1조 (목적)<br/> 본 약관은 코렐브랜드코리아 주식회사 (이하 "회사")가 제공하는 모든 서비스(이하 "서비스"라고 하며, 접속 가능한 유·무선 단말기의 종류와는 상관없이 이용 가능한 "회사"가 제공하는 모든 "서비스"를 의미합니다. 이하 같습니다)와 관련하여 회사와 이용자의 제반 권리와 의무, 기타 필요한 사항을 정함을 목적으로 합니다.<br/><br/> 제2조(용어의 정의) <br/> 1. 본 약관의 주요 용어는 아래와 같이 정의합니다.<br/> ① "코렐브랜드"는 코렐브랜드코리아 주식회사를 말합니다.<br/> ② "서비스"란 "코렐브랜드"가 인터넷 및 모바일에서 제공하는 한국코렐브랜드사이트 관련 서비스를 말합니다.<br/> ③ "이용자"란 "서비스"를 이용하는 회원 및 비회원을 말합니다.<br/> ④ "회원"은 "코렐브랜드"에 개인 정보를 제공하여 회원 등록을 한 자로서, 회원 전용 "서비스"를 이용할 수 있는 자를 말합니다.<br/> ⑤ "비회원"이라 함은 회원에 가입하지 않고 "서비스"를 이용하는 자를 말합니다.<br/> "게시물"이란 서비스를 이용함에 있어서 이용자가 올린 글, 이미지, 동영상, 각종 링크, 각종 댓글 등의 정보를 말합니다. <br/> ⑥ "아이디"란 회원식별과 서비스 이용을 위하여 이용자가 입력하고 회사가 인정한 사용자명을 말합니다. <br/> ⑦ "비밀번호"란 이용자의 정보보호와 안전한 서비스 이용을 위하여 이용자가 선정한 문자와 숫자의 조합을 말합니다. <br/> 2. 본 약관에서 사용하는 용어의 정의는 제1항에서 정하는 것을 제외하고는 관계법령 정하는 바에 의합니다.<br/><br/> 제3조(약관의 개정)<br/> ① 회사는 관련 법령에 위배되지 않는 범위 내에서 본 약관을 개정할 수 있습니다. <br/> ② 회사는 개정약관의 적용일자 7일 전에 그 적용일자, 개정항목, 개정사유를 명시하여, 전자우편 발송, 휴대폰문자서비스 발송, 서면 발송, 코렐브랜드 웹사이트 게시 중 1개 이상의 방법으로 회원에게 고지합니다. <br/> ③ 개정약관에 동의하지 않는 회원은 이용계약을 해지할 수 있습니다. 적용일자까지 이용계약을 해지하지 않은 회원은 개정약관에 동의한 것으로 간주합니다. <br/><br/> 제4조(서비스 이용계약의 성립) <br/> ① 서비스이용 계약은 회원이 되고자 하는 자가 회사가 정한 절차에 따라 회원가입 신청을 하고 회사가 이에 승낙함으로써 성립합니다. <br/> ② 회사는 다음 각 호에 해당하는 경우에는, 회원가입 신청을 승낙하지 않거나 사후에 서비스이용 계약을 해지할 수 있습니다. <br/> 1. 신청자가 본 약관에 의하여 이전에 회원자격을 상실한 경우. 다만 회원자격 상실 후 6개월이 경과한 자로서 회사로부터 회원 재가입 승낙을 얻은 경우는 예외로 함. <br/> 2. 다른 사람의 명의를 사용하거나, 본인의 실명으로 신청하지 않은 경우 <br/> 3. 허위의 정보를 기재하거나, 회사가 제시하는 내용을 기재하지 않은 경우 <br/> 4. 관련 법령 또는 공서양속에 반하거나 기타 부정한 목적으로 신청한 경우 <br/> 5. 기타 신청자의 귀책사유로 승낙이 곤란한 경우 <br/> ③ 회사는 외국인 이용자, 법인 이용자에 대하여 일정한 서비스를 제한할 수 있습니다. <br/> ④ 회원가입 신청의 경우, 회사는 회원의 종류에 따라 전문기관을 통한 실명확인 및 본인인증을 요청할 수 있습니다. <br/> ⑤ 회사는 회사정책에 따라 회원을 등급별로 구분하여 서비스에 차등을 둘 수 있습니다. <br/><br/> 제5조(회사의 의무) <br/> ① 회사는 관련 법령, 공서양속과 본 약관이 금지하는 행위를 하지 않으며, 지속적이고 안정적인 서비스 제공을 위하여 노력합니다. <br/> ② 회사는 회원이 안전하게 서비스를 이용할 수 있도록 개인정보 보호를 위한 보안시스템을 갖추며, 개인정보취급방침을 공시하고 준수합니다. <br/> ③ 회사는 서비스 이용과 관련하여 회원으로부터 제기된 의견이나 불만이 정당하다고 인정할 경우에는 최대한 신속하게 이를 처리합니다. 이 때, 회사는 회원이 제기한 의견이나 불만사항의 처리 과정과 결과를 전자우편, 전화, 휴대폰문자서비스, 서면 등을 통하여 회원에게 통지합니다. <br/><br/> 제6조(회원의 의무)<br/> 회원은 서비스 이용과 관련하여 다음 각 호의 행위를 하여서는 안 됩니다. <br/> 1. 서비스 이용 관련 제반 신청 또는 변경 시 허위내용 기재 행위<br/> 2. 코렐브랜드 서비스에 게시된 각종 정보의 무단 변경, 삭제, 훼손 행위 <br/> 3. 회사가 허용한 정보 이외의 다른 정보(컴퓨터 프로그램 및 광고 등)를 송신하거나 게시하는 행위 <br/> 4. 회사 또는 제3자의 저작권 등 권리를 침해하는 행위 <br/> 5. 회사 또는 제3자의 명예, 프라이버시를 침해하거나 업무를 방해하는 행위 <br/> 6. 외설 또는 폭력적인 정보, 기타 공서양속에 반하는 정보를 코렐브랜드주식회사 서비스에 공개하거나 게시하는 행위<br/><br/> 제7조(개인정보보호)<br/> 회사는 관련 법령과 회사의 개인정보취급방침을 준수하고 이에 따라 회원의 개인정보를 보호합니다. 다만, 코렐브랜드주식회사 웹사이트에 링크된 외부 웹사이트에서는 회사의 개인정보취급방침이 적용되지 않습니다. <br/><br/> 제8조(아이디 및 비밀번호)<br/> ① 회원이 되고자 하는 자는 회사가 정하는 바에 따라 아이디 및 비밀번호를 코렐브랜드주식회사 웹사이트에 등록하여야 합니다. <br/> ② 아이디와 비밀번호에 관한 관리책임은 회원 본인에게 있습니다. <br/> ③ 회원은 본인의 아이디 또는 비밀번호를 제3자에게 알려주거나 이용하게 해서는 안 됩니다. <br/> ④ 회원은 본인의 아이디 또는 비밀번호를 도난 당하거나 제3자가 사용하고 있음을 인지한 경우에는 즉시 회사에 통보하고 회사의 안내가 있는 경우에는 이에 따라야 합니다. <br/> ⑤ 회원으로부터 제4항의 통보를 받은 경우에는 회사는 즉시 필요한 제반 조치를 취합니다. 단, 회사는 통보를 받기 전까지 발생한 손해에 대하여는 어떠한 책임도 지지 않습니다. <br/><br/> 제9조(서비스의 이용)<br/> ① 회원으로 가입하면 모든 코렐브랜드주식회사 서비스에 동일한 아이디로 자동 가입되며, 해당 서비스를 이용할 수 있습니다. <br/> ② 서비스 이용은 회사의 업무상 또는 기술상 특별한 지장이 없는 한 연중무휴, 1일 24시간을 원칙으로 합니다. 다만, 서비스를 일정한 범위로 분할하여 각 범위 별로 이용 가능한 시간을 별도로 정할 수 있으며, 이 경우에는 그 내용을 코렐브랜드주식회사 서비스에 공지합니다. <br/> ③ 회사는 정보통신시스템 등의 정기점검, 보수, 교체, 고장, 통신두절 등의 사유가 발생한 경우에는 서비스의 제공을 일시 중단할 수 있습니다. 이 경우 회사는 그 내용을 코렐브랜드주식회사 서비스에 공지합니다. 다만, 사전에 공지할 수 없는 부득이한 사유가 있는 경우에는 사후에 공지합니다.<br/><br/> 제10조(서비스의 내용 및 변경)<br/> ① 회사가 이용자에게 제공하는 서비스는 다음과 같습니다. <br/> 1. 코렐브랜드주식회사의 제품정보 제공 <br/> 2. 이벤트 등 광고성 정보의 제공 <br/> 3. 기타 회사가 개발하거나 다른 회사와의 제휴계약 등을 통해 제공하는 일체의 서비스 <br/> ② 회사는 서비스 개선, 추가 서비스의 제공 등을 위하여 필요한 경우 회원의 동의를 받아 추가적인 개인정보를 수집할 수 있습니다. <br/> ③ 회사는 무료로 제공하는 서비스의 일부 또는 전부를 회사의 정책 및 운영의 필요상 수정, 중단, 변경할 수 있으며, 관련 법령에 특별한 규정이 없는 한 이용자에게 별도의 보상을 하지 않습니다.<br/><br/> 제11조(서비스이용 제한)<br/> ① 다음 각 호의 사유에 해당하는 경우, 회사는 서비스 이용을 제한 또는 정지할 수 있습니다. <br/> 1. 다른 이용자의 코렐브랜드주식회사 서비스 이용을 방해하는 경우 <br/> 2. 다른 이용자의 정보를 도용하는 경우 <br/> 3. 코렐브랜드주식회사 서비스를 이용하여 관련 법령이나 공서양속에 반하는 행위를 하는 경우 <br/> 4. 기타 본 약관에 규정된 의무를 위반한 경우 <br/> ② 회사가 서비스 이용을 제한 또는 정지하고자 할 때에는 미리 그 사유, 일시, 기간을 전자우편, 전화, 휴대폰문자서비스, 서면 등의 방법으로 통지합니다. 다만 긴급을 요할 경우에는 조치 후에 통지할 수 있습니다. <br/> ③ 회사가 서비스 이용을 제한 또는 정지시킨 후 그 제재사유가 된 행위가 반복되거나 30일 이내에 그 사유가 시정되지 아니하는 경우 회사는 회원자격 박탈 또는 영구적인 이용정지를 할 수 있습니다. 본 항에 따른 회원자격 박탈 또는 영구적인 이용정지 시 서비스 이용을 통해 획득한 포인트 및 기타 혜택도 모두 소멸되며, 회사는 이에 대해 별도로 보상하지 않습니다. <br/> ④ 회사가 회원자격을 박탈하는 경우에는 회원등록을 말소하며, 이 경우 말소 전에 회원에게 이를 통지합니다. <br/> ⑤ 이용자는 본 조에 따른 이용 제한 등에 대해 회사가 정한 절차에 따라 이의신청을 할 수 있습니다. 이의가 정당하다고 회사가 인정하는 경우 회사는 즉시 서비스 이용을 재개합니다. <br/><br/> 제12조(저작권 등)<br/> ① 이용자가 작성한 게시물은 저작권법에 의해 보호를 받습니다. <br/> ② 회사가 작성한 저작물, 서비스 및 그에 필요한 소프트웨어, 이미지, 마크, 로고, 디자인, 명칭, 정보, 상표 등에 대한 저작권 기타 지적재산권은 회사에 귀속합니다. <br/> ③ 이용자는 코렐브랜드주식회사 서비스를 통하여 얻은 정보를 회사의 사전 승낙 없이 복제, 송신, 출판, 배포, 방송 기타 방법에 의하여 영리목적으로 이용하거나 제3자에게 이용하게 하여서는 안 됩니다. <br/> ④ 회사는 이용자의 사전 동의를 받아 이용자가 작성한 게시물을 상업적 목적으로 사용할 수 있습니다. 이 때, 회사는 게시물의 출처를 표시하며, 게시물 사용에 동의한 이용자에게 별도의 보상을 제공합니다. <br/><br/> 제13조(게시물 관리)<br/> ① 이용자가 작성한 게시물로 인해 발생하는 모든 책임은 이용자 본인에게 귀속합니다. <br/> ② 이용자는 다음 각 호에 해당하는 게시물을 게시하거나 전달할 수 없으며, 회사는 서비스 내에 존재하는 게시물이 다음 각 호에 해당한다고 판단되는 경우 해당 게시물을 삭제, 이동 또는 등록거부를 할 수 있습니다. 또한, 회사는 게시물에 관한 세부 이용지침을 별도로 정하여 시행할 수 있으며, 이용자는 그 지침에 따라 각종 게시물을 등록하거나 삭제하여야 합니다. <br/> 1. 회사 또는 제3자의 저작권 등 권리를 침해하는 내용인 경우 <br/> 2. 회사 또는 제3자를 비방하거나 명예를 손상시키는 내용인 경우 <br/> 3. 관련 법령이나 공서양속을 위반하는 경우 <br/> 4. 승인되지 않은 광고, 판촉물을 게재하는 경우 <br/> 5. 타인의 개인정보를 도용하여 작성한 내용이거나, 타인이 입력한 정보를 무단으로 위·변조한 내용인 경우 <br/> 6. 동일한 내용을 중복하여 다수 게시하는 등 게시의 목적에 어긋나는 경우 <br/> 7. 기타 회사가 정한 이용지침 등에 위반된다고 판단되는 경우 <br/> ③ 이용자가 회원탈퇴를 하거나 서비스이용 계약이 해지된 경우 해당 이용자 계정에 기록된 게시물은 모두 삭제됩니다. 다만, 제3자에게 의하여 스크랩, 주소복사, 소스복사 등으로 다시 게시된 게시물, 제3자에 의하여 댓글이 첨부된 게시물 등 다른 이용자의 정상적인 서비스 이용에 필요한 게시물은 삭제되지 않습니다. <br/> ④ 회사는 합병, 영업양도, 코렐브랜드주식회사 웹사이트 통합 등의 사유가 발생한 경우, 원래의 게시물의 내용을 변경하지 않고 게시물의 위치를 변경할 수 있습니다. <br/><br/> 제14조(회원정보의 변경 및 이용계약 해지)<br/> ① 회원은 코렐브랜드주식회사 서비스 내에서 MY KITCHEN 메뉴를 통하여 언제든지 본인의 개인정보를 열람하고 수정할 수 있습니다. 다만, 서비스 관리를 위해 필요한 실명, 아이디(이메일 주소)는 수정이 불가능합니다.<br/> ② 회원은 회원가입 시 기재한 사항이 변경되었을 경우에는 제1항에 따라 수정을 하거나 전자우편 등의 방법으로 회사에게 그 변경사항을 알려야 합니다. <br/> ③ 변경사항을 회사에 알리지 않아 발생한 불이익에 대하여는 회사는 책임지지 않습니다. <br/> ④ 회원은 언제든지 코렐브랜드주식회사 홈페이지 초기화면의 고객만족센터 또는 MY KITCHEN 메뉴를 통하여 서비스이용 계약 해지를 신청할 수 있으며, 회사는 관련 법령이 정하는 바에 따라 이를 즉시 처리합니다. <br/> ⑤ 회원이 서비스이용 계약을 해지하는 경우, 코렐브랜드주식회사 회원 자격과 코렐브랜드주식회사 서비스를 통하여 적립된 포인트도 함께 소멸됩니다. <br/><br/> 제15조(손해배상)<br/> 이용자의 불법행위 또는 본 약관 위반으로 인하여 회사에 손해가 발생한 경우, 해당 이용자는 회사에게 발생한 모든 손해를 배상하여야 합니다.<br/><br/> 제16조(면책조항)<br/> ① 회사는 천재지변, 전쟁, 이에 준하는 불가항력, 기타 회사의 귀책사유에 의하지 아니하고 이용자에게 발생한 손해에 대하여 책임을 지지 않습니다. <br/> ② 회사는 이용자가 코렐브랜드주식회사 서비스에 게재한 각종 정보, 자료, 사실의 신뢰도, 정확성을 보증하지 아니하며, 이와 관련하여 다른 이용자에게 발생한 손해에 대하여 책임을 지지 않습니다. <br/> ③ 회사는 이용자 상호간 및 이용자와 제3자 상호 간에 서비스를 매개로 발생한 분쟁에 대해 개입할 의무가 없으며, 이로 인한 손해에 대하여 책임을 지지 않습니다. <br/> ④ 회사는 다른 이용자의 행위로 인하여 입게 되는 이용자의 정신적 피해에 대하여는 책임을 지지 않습니다. <br/> ⑤ 회사는 무료로 제공하는 서비스 이용과 관련하여 관련 법령에 특별한 규정이 없는 한 책임을 지지 않습니다. <br/><br/> 제17조(관할법원 및 준거법)<br/> ① 서비스 이용 중 발생하는 이용자와 회사간의 분쟁에 대하여 소송이 제기될 경우, 회사의 본사 소재지를 관할하는 법원을 관할법원으로 합니다. <br/> ② 본 약관에 관한 소송은 대한민국법을 준거법으로 합니다. <br/><br/> 제18조(부칙)<br/> 1. 본 약관은 2013년 12월 31일부터 적용됩니다. <br/> 2. 2008년 3월 11일부터 시행되던 종전의 약관은 본 약관으로 대체합니다. </div></div><div id="email-policy" class="popup-term" style="display: none"> <a href="#" class="close-modal"></a> <span class="s-tit">이메일주소 수집거부</span> <div class="content"> 코렐브랜드코리아주식회사는 공식적으로 허가되지 않은 이메일 무단 수집을 거부합니다.<br/><br/> 본 웹사이트에서 게시된 이메일 주소가 전자우편 수집 프로그램이나 그 밖의 기술 장치를 이용하여 무단으로 수집되는 것을 거부하며, 이를 위반 시 정보 통신망법에 의해 형사 처벌됨을 유념하시기 바랍니다. </div></div>';
+
+let appendFooter = '';
+appendFooter += '<div class="copyright">';
+appendFooter += '       <div class="menus">';
+appendFooter += '           <span class="f">고객센터</span><br />';
+appendFooter += '           <p class="f">평일 09:00 - 18:00 (점심시간 : 12:00 - 13:30)<br />';
+appendFooter += '           WKKCS@worldkichen.com</p>';
+appendFooter += '           <span class="s">02.2670.7800</span>';
+appendFooter += '       </div>';
+appendFooter += '       <div class="phone-time">';
+appendFooter += '           <a href="about_history.html">회사소개</a><a href="#">이용약관</a><a href="#">이메일주소 수집거부</a>';
+appendFooter += '           <p>서울 강남구 테헤란로 98길 8 8층(KT&G 대치타워 8층)<br />';
+appendFooter += '           Copyright ⓒ 2015 World Kitchen All Rights Reserved.</p>';
+appendFooter += '       </div>';
+appendFooter += '</div>';
+
+
 let CORE = {
     init : function() {
-        
+        $('header').html(appendHeader);
+        $('div.wrapper').append(appendContent);
+        $('footer').html(appendFooter);
+
         this.realTabOperation();
+        this.menuStart();
+        this.frequentlyAskedQuestions();
+        this.naviHeightFix();
     },
     // GNB indicator
-    topMenuIndicator : function() {
-        $('nav a.here').addClass('on');
+    menuStart : function() {
+        $('header a.menu').on('click', function(){
+            $('.submenu, .film-dark').fadeIn('fast');
+        });
+        $('.submenu ul.navi a').on('click', function(){
+            let thisData = $(this).data('gnbMenu');
+            $('.depthmenu').fadeIn('fast');
+            switch(thisData) {
+                case 'about' :
+                    $('.depthmenu span.depth-1').text('About Us');
+                break;
+                case 'brand' :
+                    $('.depthmenu span.depth-1').text('Brands');
+                break;
+                case 'product' :
+                    $('.depthmenu span.depth-1').text('Product');
+                break;
+                case 'support' :
+                    $('.depthmenu span.depth-1').text('Support');
+                break;
+                case 'news' :
+                    $('.depthmenu span.depth-1').text('News & Event');
+                break;
+                case 'webzine' :
+                    $('.depthmenu span.depth-1').text('Webzine');
+                break;
+            }
+            $('.depthmenu ul.navi').css('display','none');
+            $('.depthmenu #menu-'+thisData).css('display','block');
+        });
+        $('.depthmenu a.home').on('click', function(){
+            $('.depthmenu').fadeOut('fast');
+        });
+        $('.film-dark, .submenu a.close-modal, .depthmenu a.close-modal').on('click', function(){
+            $('.submenu, .depthmenu, .film-dark').fadeOut('fast');
+        });
+
+        //copyright
+        $('.copyright .phone-time a:nth-child(2)').on('click', function(){ //이용약관
+            $('#use-term').fadeIn('fast'); 
+        });
+        $('.copyright .phone-time a:nth-child(3)').on('click', function(){ //이메일 정책
+            $('#email-policy').fadeIn('fast'); 
+        });
+        $('div.popup-term a.close-modal').on('click', function(){
+            $('#use-term, #email-policy').fadeOut('fast'); 
+        });
+        $('h1').on('click', function(){
+            location.href = '/';
+        });
+    },
+
+    // FAQ
+    frequentlyAskedQuestions : function() {
+        $('.questions a.question-link').on('click', function(e){
+            e.preventDefault();
+            if(!$(this).hasClass('on')) {
+                $('.questions div.answer').slideUp(300);    
+            }
+            $('.questions a.question-link').removeClass('on');
+            $(this).addClass('on');
+            $(this).next('.answer').slideDown(300);
+        });
     },
 
     // TAB plug
@@ -16,6 +174,14 @@ let CORE = {
             $(this).addClass('on');
         });
     },
+
+    // Navigatoin fixed size
+    naviHeightFix: function() {
+        let wrapper = $(window).height();
+        let margintp = wrapper - 122; // 122 depth menu header's heihgt
+        $('div.depthmenu div.inner').css('height', margintp);
+    }
+
 }
 
 
